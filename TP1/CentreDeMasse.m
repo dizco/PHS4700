088dd9da134982178f90600db24de22495ab3bfb
kevin <ka-hin-kevin.chan@polymtl.ca>
2017-09-21 15:44:11 -0400

    restructuration & optimisation fonction CM navette

--- TP1/CentreDeMasse.m
+++ TP1/CentreDeMasse.m
@@ -15,22 +15,18 @@ centre = [0, 0, hauteur/4];
 end
 
 
-function [centre] = CentreDeMasseObjets(cm, masses)
+function [centreTotal] = CentreDeMasseObjets(centreObjets, massesObjets)
     
-masseTotale = sum(masses);
+masseTotale = sum(massesObjets);
 
-centre = [0;0;0];
+centreTotal = [0;0;0];
 
-for i = 1:numel(masses)
-    x = cm(i, 1);
-    y = cm(i, 2);
-    z = cm(i, 3);
-    centreI = [x ; y ; z];
-    poids = masses(i) / masseTotale;
-    centreI = centreI * poids;
-    
-    centre = centre + centreI;
-end
+for i = 1:numel(massesObjets)
+    %x = element numero 1, y = element numero 2, z = element numero 3;
+    centreObjetPondere = [centreObjets(i, 1) ; centreObjets(i, 2) ; centreObjets(i, 3)] * massesObjets(i);
+    centreTotal = centreTotal + centreObjetPondere;
+end %fin boucle for
+centreTotal = centreTotal / masseTotale;
 
 end
 
