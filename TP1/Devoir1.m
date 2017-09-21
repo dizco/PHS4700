aed5bb870d1a187b3f6c7a1c5ade861fc828ac29
dizco <gabriel.bourgault@kiosoft.ca>
2017-09-21 19:10:42 -0400

    Merge

--- TP1/Devoir1.m
+++ TP1/Devoir1.m
@@ -5,7 +5,15 @@ Donnees();
 pcmNL = navette.CentreDeMasse;
 
 INL = 0;
-alphaNL = [0;0;0];
+alphaNL = [0,0,0];
+
+if (AngRot ~= 0)
+    % TODO: Cas 2
+end
+
+if (vangulaire ~= 0)
+    % TODO: Cas 2
+end
     
 end
 
