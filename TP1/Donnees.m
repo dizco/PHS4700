6cbe7b0b091e95108fa2c5a69440e5d0a7a29f4a
dizco <gabriel.bourgault@kiosoft.ca>
2017-09-23 08:59:34 -0400

    Fix centre de masse Z du reservoir

--- TP1/Donnees.m
+++ TP1/Donnees.m
@@ -27,14 +27,18 @@ hautReservoir.Rayon = 4.2;
 hautReservoir.Hauteur = 7.8;
 hautReservoir.CentreDeMasse = CentreDeMasse.CentreDeMasseCone(hautReservoir.Rayon, hautReservoir.Hauteur);
 
-reservoir = FormeFusee(basReservoir, hautReservoir);
-reservoir.CoordonneesBasMilieu(0, basNavette.Rayon + basReservoir.Rayon, 0);
-
 masseHydrogeneBasReservoir = 108000;
 volumeOxygeneBasReservoir = pi * (4.2 ^ 2) * (39.1 - 46.9 * (2/3)); % pi * R^2 * h
 masseOxygeneBasReservoir = 631000 * volumeOxygeneBasReservoir / (volumeOxygeneBasReservoir + hautReservoir.CalculerVolume());
 masseOxygeneHautReservoir = 631000 - masseOxygeneBasReservoir;
 
+cmCyl1 = CentreDeMasse.CentreDeMasseCylindre(basReservoir.Rayon, 46.9 * (2/3));
+cmCyl2 = CentreDeMasse.CentreDeMasseCylindre(basReservoir.Rayon, 39.1 - 46.9 * (2/3));
+cmCyl2(3) = cmCyl2(3) + 46.9 * (2/3);
+basReservoir.CentreDeMasse = CentreDeMasse.CentreDeMasseObjets([cmCyl1; cmCyl2], [masseHydrogeneBasReservoir; masseOxygeneBasReservoir]);
+
+reservoir = FormeFusee(basReservoir, hautReservoir);
+reservoir.CoordonneesBasMilieu(0, basNavette.Rayon + basReservoir.Rayon, 0);
 reservoir.RepartirMasseParComposante(masseHydrogeneBasReservoir + masseOxygeneBasReservoir, masseOxygeneHautReservoir);
 
 % Propulseur
