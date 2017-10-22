a0cab926aaef1d21458f0daa40bc7e70854e13ce
Gabriel B <bour31@hotmail.com>
2017-10-22 14:45:41 -0400

    Cleanup loop de rk4 et g1

--- TP2/g1.m
+++ TP2/g1.m
@@ -1,12 +1,3 @@
 function g = g1 (q0, t0)
-%disp('g q0')
-%disp(q0);
-%disp('g t0');
-%disp(t0);
-
-%f = Calculs.gravite(balle);
-%a = f / balle.Masse;
-a = [0; 0; -9.8]; %TODO: Calculer accélération
-%g = [q0(1) q0(2) q0(3)]; %TODO: Appliquer ici gravité
 
 g = [0 0 -9.8 q0(1) q0(2) q0(3)];
