bab8dd6256d7af79879c25668b8244482b185044
kevin <ka-hin-kevin.chan@polymtl.ca>
2017-11-02 10:47:51 -0400

    bug fonction print du main résolu

--- TP3/main.m
+++ TP3/main.m
@@ -8,36 +8,37 @@ disp('TP3');
 % Tir 1
 figure;
 [Coll, tf, raf, vaf, rbf, vbf] = Devoir3([0 0], [20 0 2], [100 100], [0 -20 -1], 0.0);
-print(Coll, tf, raf, vaf, rbf, vbf);
+print(Coll, tf, raf, vaf, rbf, vbf, 1);
 
 % Tir 2
 figure;
 [Coll, tf, raf, vaf, rbf, vbf] = Devoir3([0 0], [30 0 2], [100 100], [0 -30 -1], 0.0);
-print(Coll, tf, raf, vaf, rbf, vbf);
+print(Coll, tf, raf, vaf, rbf, vbf, 2);
 
 %Tir 3
 figure;
 [Coll, tf, raf, vaf, rbf, vbf] = Devoir3([0 0], [20 0 2], [100 50], [0 -10 0], 1.6);
-print(Coll, tf, raf, vaf, rbf, vbf);
+print(Coll, tf, raf, vaf, rbf, vbf, 3);
 
 % Tir 4
 figure;
 [Coll, tf, raf, vaf, rbf, vbf] = Devoir3([0 0], [10 10 1], [25 10], [10 0 0], 0.0);
-print(Coll, tf, raf, vaf, rbf, vbf);
+print(Coll, tf, raf, vaf, rbf, vbf, 4);
 
 %Tir 5
 figure;
 [Coll, tf, raf, vaf, rbf, vbf] = Devoir3([0 0], [20 0 2], [100 50], [0 -10 0], 0.0);
-print(Coll, tf, raf, vaf, rbf, vbf);
+print(Coll, tf, raf, vaf, rbf, vbf, 5);
 
 %Tir 6
 figure;
 [Coll, tf, raf, vaf, rbf, vbf] = Devoir3([0 0], [20 2 2], [100 10], [10 0 5], 1.0);
-print(Coll, tf, raf, vaf, rbf, vbf);
+print(Coll, tf, raf, vaf, rbf, vbf, 6);
 
-%{
-function print(Coll, tf, raf, vaf, rbf, vbf)
-    disp('-----');
+
+function print(Coll, tf, raf, vaf, rbf, vbf, i)
+    affichage = ['-----TIR ', num2str(i), '------'];
+    disp(affichage);
     fprintf('Collision = %s\n', stringCollision(Coll));
     fprintf('Temps = %d\n', tf);
     
@@ -58,5 +59,5 @@ function str = stringCollision(coll)
         str = 'Vrai';
     end
 end
-%}
+
 
