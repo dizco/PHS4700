bf81c90fa054bd362061533248efb0f23dd80831
dizco <gabriel.bourgault@kiosoft.ca>
2017-11-04 14:13:41 -0400

    Entre données initiales des véhicules dans la fct Donnees

--- TP3/Devoir3.m
+++ TP3/Devoir3.m
@@ -16,23 +16,10 @@ function [Coll, tf, raf, vaf, rbf, vbf] = Devoir3(rai, vai, rbi, vbi, tb)
 %   tf temps de la fin de la simulation (s)
 %   rbf vecteur positions finales du cm de la balle (m)
 %   vbf vecteur vitesse finale du cm de la balle (m/s)
-
-    fonctionG = 'g1';
     
-	systeme = Donnees();
+	systeme = Donnees(rai, vai, rbi, vbi);
     
-    pas = 0.01; %variation de temps à chaque itération
-    
-    % Input les vitesses initiales
-    systeme.AutoA.Position = [rai(1) rai(2)];
-  
-    
-    systeme.AutoB.Position = [rbi(1) rbi(2)];
-    systeme.AutoA.Vitesse = [vai(1) vai(2)];
-    systeme.AutoA.VitesseAngulaire = vai(3);
-    systeme.AutoB.Vitesse = [vbi(1) vbi(2)];
-    systeme.AutoB.VitesseAngulaire = vbi(3);
-  
+    pas = 0.1; %variation de temps à chaque itération  
     
     tf = 0;
     raf = [0; 0];
@@ -47,39 +34,39 @@ function [Coll, tf, raf, vaf, rbf, vbf] = Devoir3(rai, vai, rbi, vbi, tb)
     qsA = [systeme.AutoA.Vitesse(1) systeme.AutoA.Vitesse(2) 0 systeme.AutoA.Position(1) systeme.AutoA.Position(2) 0];
     qsB = [systeme.AutoB.Vitesse(1) systeme.AutoB.Vitesse(2) 0 systeme.AutoB.Position(1) systeme.AutoB.Position(2) 0];
    
-    while 1 %TODO: Loop infinie jusqu'à collision
-        qsA = SEDRK4(qsA, 0, tempsEcoule + pas, 'frottement', systeme.AutoA);
-        if(tempsEcoule >= tb)
-            qsB = SEDRK4(qsB, 0, tempsEcoule + pas, 'frottement', systeme.AutoB);
-        else 
-            qsB = SEDRK4(qsB, 0, tempsEcoule + pas, 'rouler', systeme.AutoB);
-        end    
-        % TODO: Runge kutta pour A et B
+    while 1 %Loop infinie jusqu'à collision
         
+        qsA = SEDRK4(qsA, 0, tempsEcoule, 'frottement', systeme.AutoA);
+        if(tempsEcoule >= tb) %L'auto B commence à glisser au temps tb
+            qsB = SEDRK4(qsB, 0, tempsEcoule, 'frottement', systeme.AutoB);
+        else 
+            qsB = SEDRK4(qsB, 0, tempsEcoule, 'rouler', systeme.AutoB);
+        end        
         
         positionA = Vecteur.CreateFromArray([qsA(4) qsA(5)]);
         %positionB = Vecteur.CreateFromArray([qsB(4) qsB(5)]);
-        positionB = [0 0];
+        positionB = Vecteur.CreateFromArray(systeme.AutoB.Position);
 
         %positionsA(end + 1) = [positionA.X positionA.Y]; %Push positions pour affichage
         %positionsB(end + 1) = [positionB.X positionB.Y]; %Push positions pour affichage
        
         [estCollision, pointCollision] = EtatCollision(systeme.AutoA, systeme.AutoB, positionA, positionB, tempsEcoule, tb);
         if (estCollision)
-            tf = tempsEcoule + pas;
+            tf = tempsEcoule;
             %rbf = positionBalle.GetHorizontalArray();
             %vbf = qs(1, 1:3);
-            %break;
+            break;
         end
         
         if (tempsEcoule > 10) %TODO: Remove
             disp('Error: Too many iterations. Simulation ended.');
             break;
         end
-
+        
         tempsEcoule = tempsEcoule + pas;
+        
     end
-    print('Simulation');
+    %print('Simulation');
     
     %AutoA = Auto();
     %coinsA = getCoinsAutoSansRotation(AutoA, rai)
--- TP3/Donnees.m
+++ TP3/Donnees.m
@@ -1,16 +1,22 @@
-function systeme = Donnees() 
-
+function systeme = Donnees(rai, vai, rbi, vbi) 
+    
     autoA = Auto();
     autoA.Masse = 1540;
     autoA.Longueur = 4.78;
     autoA.Largeur = 1.82;
     autoA.Hauteur = 1.8;
+    autoA.Position = [rai(1) rai(2)];
+    autoA.Vitesse = [vai(1) vai(2)];
+    autoA.VitesseAngulaire = vai(3);
     
     autoB = Auto();
     autoB.Masse = 1010;
     autoB.Longueur = 4.23;
     autoB.Largeur = 1.6;
     autoB.Hauteur = 1.8;
+    autoB.Position = [rbi(1) rbi(2)];
+    autoB.Vitesse = [vbi(1) vbi(2)];
+    autoB.VitesseAngulaire = vbi(3);
     
     systeme = Systeme();
     systeme.AutoA = autoA;
