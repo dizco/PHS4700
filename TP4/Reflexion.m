cba2ae3c87655de86a4c4a8eefd248409ba13f29
Kenny-3 Nguyen <kengub@l4810-16.gigl.polymtl.ca>
2017-11-17 14:48:45 -0500

    Ajout des formules pour la reflexion et pour la refraction

---
+++ TP4/Reflexion.m
@@ -0,0 +1,7 @@
+% donne le vecteur unitaire (ur) sortant de la reflexion
+% ui : vecteur unitaire du 
+% ni : vecteur normale unitaire (sortant de la surface)
+function ur = Reflexion (ui, i)
+    % Eq (7.13)
+    ur = ui - 2 * dot(ui, i) * ui;
+end
---
+++ TP4/Refraction.m
@@ -0,0 +1,37 @@
+% donne le vecteur unitaire sortant (ut) de la réfraction
+% ui : vecteur unitaire entrant
+% i : vecteur unitaire normal à la surface du plan
+% ni : indice de réfraction du milieu i
+% nt : indice de réfraction du milieu t
+
+function ut = Refraction(ui, i, ni, nt)
+
+%vecteurs unitaires
+ui = ui / norm(u);
+i = i / norm(i);
+
+angleCritique = abs(asin(nt/ni));
+
+% Eq (7.10)
+j = cross(ui, i);
+% Eq (7.11)
+k = cross(n, j);
+
+% Eq (7.30)
+sini = dot(ui, k);
+% Angle d'incidence
+anglei = asin(sini);
+
+    if -angleCritique <= anglei && anglei <= angleCritique
+        % Eq (7.31)
+        sint = ni/n2 * sini;
+        
+        % cos^2 + sin^2 = 1
+        % cos = (1 - sin^2)^(1/2)
+        
+        % Eq (7.36)
+        ut = -i * (1 - sint^2)^(1/2) + k * sint;
+    else
+        ut = [0 0 0]';
+    end
+end
