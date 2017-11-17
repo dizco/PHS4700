1be353b0317f7774629eab99dca69b89f4d8f8a2
dizco <bour31@hotmail.com>
2017-11-17 15:55:41 -0500

    Ajout de tous les cas dans le main

--- TP4/main.m
+++ TP4/main.m
@@ -5,4 +5,14 @@ close all;
 
 disp('Devoir 4');
 
+%Cas 1
 [xi, yi, zi, face] = Devoir4(1, 1, [0, 0, 5]);
+
+%Cas 2
+%[xi, yi, zi, face] = Devoir4(1, 1.5, [0, 0, 5]);
+
+%Cas 3
+%[xi, yi, zi, face] = Devoir4(1, 1.5, [0, 0, 0]);
+
+%Cas 4
+%[xi, yi, zi, face] = Devoir4(1.2, 1, [0, 0, 5]);
