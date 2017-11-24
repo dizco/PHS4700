5cb899e4b1d7d81b58bcb6c37f453fcec34743e4
kevin <ka-hin-kevin.chan@polymtl.ca>
2017-11-24 17:13:35 -0500

    range angles verticaux fonctionne

--- TP4/DroiteAleatoireSpec.m
+++ TP4/DroiteAleatoireSpec.m
@@ -1,6 +1,13 @@
+clear;
+clc;
+clf;
+close all;
+
 %pour tester l'IntervalleAnglesPossibles
 systeme = Donnees();
 pointObservateur = Vecteur(0, 0, 0);
+pointObs2 = Vecteur(0, 0, 0.05);
 TrouverSecteurDepart(pointObservateur, systeme);
+TrouverSecteurDepart(pointObs2, systeme);
 %DroiteAleatoire(pointObservateur, systeme);
 %-------------
--- TP4/TrouverSecteurDepart.m
+++ TP4/TrouverSecteurDepart.m
@@ -1,40 +1,36 @@
 function TrouverSecteurDepart(pointObservateur, systeme)
     rayon = systeme.CylindreTransparent.Rayon;
-    hauteur = systeme.CylindreTransparent.Hauteur;
+    demiHauteur = systeme.CylindreTransparent.Hauteur / 2;
     centre = systeme.CylindreTransparent.Centre;
-    coinVertical1 = [2 2 2];
-    coinVertical2 = [6 6 2];
-    coinVertical3 = [2 2 20];
-    coinVertical4 = [6 6 20];
+    coinVertical1 = [centre(1) - rayon      centre(2) - rayon    centre(3) - demiHauteur];
+    coinVertical2 = [centre(1) + rayon      centre(2) + rayon    centre(3) - demiHauteur];
+    coinVertical3 = [centre(1) - rayon      centre(2) - rayon    centre(3) + demiHauteur];
+    coinVertical4 = [centre(1) + rayon      centre(2) + rayon    centre(3) + demiHauteur];
     segmentVertical1 = [coinVertical1(1) - pointObservateur.X; coinVertical1(2) - pointObservateur.Y; coinVertical1(3) - pointObservateur.Z];
     segmentVertical2 = [coinVertical2(1) - pointObservateur.X; coinVertical2(2) - pointObservateur.Y; coinVertical2(3) - pointObservateur.Z];
     segmentVertical3 = [coinVertical3(1) - pointObservateur.X; coinVertical3(2) - pointObservateur.Y; coinVertical3(3) - pointObservateur.Z];
     segmentVertical4 = [coinVertical4(1) - pointObservateur.X; coinVertical4(2) - pointObservateur.Y; coinVertical4(3) - pointObservateur.Z];
-    disp(segmentVertical1);
-    disp(segmentVertical2);
-    disp(segmentVertical3);
-    disp(segmentVertical4);
-    angle1 = coinVertical1(1) / coinVertical1(3);
-    angle2 = coinVertical2(1) / coinVertical2(3);
-    angle3 = coinVertical3(1) / coinVertical3(3);
-    angle4 = coinVertical4(1) / coinVertical4(3);
-    angle1 = atand(angle1);
-    angle2 = atand(angle2);
-    angle3 = atand(angle3);
-    angle4 = atand(angle4);
-    disp("angle");
-    disp(angle1);
-    disp(angle2);
-    disp(angle3);
-    disp(angle4);
-    angleZMin = 0;
-    if angle1 < angle2
-        angleZMin = angle1;
-    else
-        angleZMin = angle2;
-    end
-    disp(angleZMin);
+    rapport1 = segmentVertical1(1) / segmentVertical1(3);
+    rapport2 = segmentVertical2(1) / segmentVertical2(3);
+    rapport3 = segmentVertical3(1) / segmentVertical3(3);
+    rapport4 = segmentVertical4(1) / segmentVertical4(3);
+    angle1 = atand(rapport1);
+    angle2 = atand(rapport2);
+    angle3 = atand(rapport3);
+    angle4 = atand(rapport4);
+    angles = [angle1 angle2 angle3 angle4];
+    angleZMin = angles(1);
     angleZMax = 0;
+    for i = 2:4
+        if angles(i) < angleZMin
+            angleZMin = angles(i);
+        else
+            angleZMax = angles(i);
+        end
+    end
+    disp("angle min :");
+    disp(angleZMin);
+    disp("angle max :");
     disp(angleZMax);
 end
 
