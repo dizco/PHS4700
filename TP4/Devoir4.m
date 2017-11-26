0e22bc1d5460a4242622ca281f13b96ad0fb8618
dizco <gabriel.bourgault@kiosoft.ca>
2017-11-25 20:16:46 -0500

    Affichage du cylindre

--- TP4/Devoir4.m
+++ TP4/Devoir4.m
@@ -7,11 +7,31 @@ function [xi, yi, zi, face] = Devoir4(nout, nin, poso)
     
     systeme = Donnees();
     
-    positionPhoton = Vecteur.CreateFromArray(poso);
+    %positionPhoton = Vecteur.CreateFromArray(poso);
     
-    droite = DroiteAleatoire(pointObservateur, systeme);
+    %droite = DroiteAleatoire(pointObservateur, systeme);
+    
+    positionPhoton = Vecteur(0.03, 0.04, 0);
+    
+    droite = Droite();
+    droite.Point = Vecteur(0.03, 0.04, 0);
+    droite.Pente = Vecteur(-1, 0, 0);
+    
+    AfficherSimulationVisuelle();
+    AfficherCylindre(systeme.CylindreTransparent);
     
     [intersectionCylindreExiste, positionIntersectionCylindre, normaleIntersectionCylindre] = CollisionCylindre(droite, positionPhoton, systeme.CylindreTransparent);
+    disp('normale intersection');
+    disp(normaleIntersectionCylindre);
+    disp('pos intersection');
+    disp(positionIntersectionCylindre);
+    hold on;
+    plot3(positionIntersectionCylindre.X, positionIntersectionCylindre.Y, positionIntersectionCylindre.Z, '*b');
+    hold on;
+    plot3(droite.Point.X, droite.Point.Y, droite.Point.Z, '*g');
+    disp('normale');
+    disp(normaleIntersectionCylindre);
+    
     if (intersectionCylindreExiste)
         [nouvelleDroite, estRefracte] = Refraction(droite.Pente.GetHorizontalArray(), normaleIntersectionCylindre, nout, nin);
         if (~estRefracte)
@@ -20,5 +40,31 @@ function [xi, yi, zi, face] = Devoir4(nout, nin, poso)
     end
     
     
+end
+
+function AfficherSimulationVisuelle()
+    figure;
+    xlabel('X (m)');
+    ylabel('Y (m)');
+    zlabel('Z (m)');
+    xlim([0.01 0.07]);
+    ylim([0.01 0.07]);
+    zlim([0 0.22]);
+    view(-45, 45);
+    grid minor;
+end
+
+function AfficherCylindre(cylindre)
+    hold on;
+    plot3(cylindre.Centre.X, cylindre.Centre.Y, cylindre.Centre.Z, '*r');
+    
+    [X, Y, Z] = cylinder(cylindre.Rayon);
+    Z(1, :) = cylindre.Centre.Z - cylindre.Hauteur / 2;
+    Z(2, :) = cylindre.Centre.Z + cylindre.Hauteur / 2;
+    X = X + cylindre.Centre.X;
+    Y = Y + cylindre.Centre.Y;
+    hold on;
+    surf(X, Y, Z, 'EdgeColor', 'b', 'FaceAlpha', 0, 'EdgeAlpha', 0.9);
+    hold off;
 end
 
