048cec8cf040b4ecb44c1cd3b156b8337cae046f
dizco <gabriel.bourgault@kiosoft.ca>
2017-11-26 20:42:43 -0500

    Bugfix CollisionCylindre, cas d'une droite parallèle à axe Y

--- TP4/CollisionCylindre.m
+++ TP4/CollisionCylindre.m
@@ -5,15 +5,25 @@ function [intersectionCylindreExiste, positionIntersectionCylindre, normaleInter
 
     positionIntersectionCylindre = Vecteur(0, 0, 0);
     normaleIntersectionCylindre = Vecteur(0, 0, 0);
+    intersectionCylindreExiste = true;
     
     [intersectionExiste, x] = ComposanteXCollision(droite, positionDepart, cylindre);
-    intersectionCylindreExiste = intersectionExiste;
     if (~intersectionExiste)
+        intersectionCylindreExiste = false;
         return;
     end
     
-    y = ComposanteYCollision(droite, x);
-    z = ComposanteZCollision(droite, x);
+    if (droite.EstVertical())
+        [intersectionExiste, y] = ComposanteYCollisionVertical(droite, positionDepart, cylindre);
+        if (~intersectionExiste)
+            intersectionCylindreExiste = false;
+            return;
+        end
+        z = ComposanteZCollisionAvecY(droite, y);
+    else
+        y = ComposanteYCollision(droite, x);
+        z = ComposanteZCollisionAvecX(droite, x);
+    end
     
     if (z > cylindre.GetBorneSuperieureZ() || z < cylindre.GetBorneInferieureZ())
         intersectionCylindreExiste = false;
@@ -24,6 +34,105 @@ function [intersectionCylindreExiste, positionIntersectionCylindre, normaleInter
     normaleIntersectionCylindre = CalculerNormaleIntersection(positionIntersectionCylindre, cylindre);
 end
 
+function [intersectionExiste, x] = ComposanteXCollision(droite, positionDepart, cylindre) 
+    %Equation 1 : (x-h)^2 + (y-k)^2 = r^2
+    %Equation 2 : y = ax + b, on néglige ici la composante Z puisque le cylindre est orienté vers le haut. 
+    %En d'autres mots, on applatit la situation dans la plan XY, pour avoir un cercle et une ligne
+    
+    if (droite.EstVertical())
+        x = positionDepart.X;
+        intersectionExiste = true;
+        return;
+    end
+    
+    [a, b, c] = CoefficientsEquationQuadratique(droite, cylindre);
+    [x1, x2] = ResoudreEquationQuadratique(a, b, c);
+    
+    [intersectionExiste, x] = SelectionnerValeurComposanteValide(x1, x2, positionDepart.X, droite.Pente.X);
+end
+
+function [intersectionExiste, valeur] = SelectionnerValeurComposanteValide(c1, c2, cPositionDepart, cPente)
+    intersectionExiste = true;
+    valeur = 0;
+    
+    c1Valide = ComposanteValide(c1, cPositionDepart, cPente);
+    c2Valide = ComposanteValide(c2, cPositionDepart, cPente);
+    
+    if (c1Valide && c2Valide)
+        %Les 2 valeurs sont valides, on prend le x pour lequel on a un step positif
+        stepsC1 = (c1 - cPositionDepart) / cPente;
+        stepsC2 = (c2 - cPositionDepart) / cPente;
+
+        if (stepsC1 > 0 && stepsC1 < stepsC2)
+            valeur = c1;
+        else
+            valeur = c2;
+        end
+    elseif (c1Valide)
+        valeur = c1;
+    elseif (c2Valide)
+        valeur = c2;
+    else
+        intersectionExiste = false;
+    end 
+end
+
+function valide = ComposanteValide(x, xDepart, xDirection)
+    %Permet de valider n'importe quelle composante
+    valide = isreal(x) && ((x > xDepart && xDirection > 0) || (x < xDepart && xDirection < 0));
+end
+
+function [a, b, c] = CoefficientsEquationQuadratique(droite, cylindre)
+    penteXY = droite.PentePlanXY();
+    ordonneeAOrigine = droite.OrdonneeAOrigine();
+    a = 1 + power(penteXY, 2);
+    b = - (2 * cylindre.Centre.X) + 2 * penteXY * (ordonneeAOrigine - cylindre.Centre.Y);
+    c = power(cylindre.Centre.X, 2) + power(ordonneeAOrigine - cylindre.Centre.Y, 2) - power(cylindre.Rayon, 2);
+end
+
+function [a, b, c] = CoefficientsEquationQuadratiqueVertical(droite, cylindre)
+    %Pour une droite verticale, la situation se simplifie comme suit :
+    %Equation 1 : (x-h)^2 + (y-k)^2 = r^2
+    %Equation 2 : x = x0
+    %On trouve donc :
+    % (x0-h)^2 + (y-k)^2 - r^2= 0
+    % y^2 - 2ky + k^2 + (x0-h)^2 - r^2 = 0
+    
+    a = 1; %y^2
+    b = - (2 * cylindre.Centre.Y); %-2ky
+    c = power(cylindre.Centre.Y, 2) + power(droite.Point.X - cylindre.Centre.X, 2) - power(cylindre.Rayon, 2); %k^2 + (x0-h)^2 - r^2
+end
+
+function y = ComposanteYCollision(droite, x)
+    %y = ax+b
+    y = droite.PentePlanXY() * x + droite.OrdonneeAOrigine();
+end
+
+function [intersectionExiste, y] = ComposanteYCollisionVertical(droite, positionDepart, cylindre) 
+    %Même principe que ComposanteYCollision
+    
+    [a, b, c] = CoefficientsEquationQuadratiqueVertical(droite, cylindre);
+    [y1, y2] = ResoudreEquationQuadratique(a, b, c);
+    
+    [intersectionExiste, y] = SelectionnerValeurComposanteValide(y1, y2, positionDepart.Y, droite.Pente.Y);
+end
+
+function z = ComposanteZCollisionAvecX(droite, x)
+    %z = ax+b
+    z = droite.PentePlanXZ() * x + droite.ValeurZPourX0();
+end
+
+function z = ComposanteZCollisionAvecY(droite, y)
+    %z = ax+b
+    z = droite.PentePlanYZ() * y + droite.ValeurZPourY0();
+end
+
+function  [x1, x2] = ResoudreEquationQuadratique(a, b, c)
+    %x = (-b +- (b^2 - 4ac)^(1/2)) / 2a
+    
+    x1 = (-b + sqrt(power(b, 2) - 4*a*c)) / (2*a);
+    x2 = (-b - sqrt(power(b, 2) - 4*a*c)) / (2*a);
+end
 
 function normale = CalculerNormaleIntersection(positionIntersection, cylindre)
     %Utiliser la position (x, y) du centre du cylindre afin de déterminer la normale
@@ -37,62 +146,4 @@ function normale = CalculerNormaleIntersection(positionIntersection, cylindre)
     
     vecteurDirecteur = pos2D - centre2D;
     normale = vecteurDirecteur / norm(vecteurDirecteur);
-end
-
-
-function [intersectionExiste, x] = ComposanteXCollision(droite, positionDepart, cylindre) 
-    %Equation 1 : (x-h)^2 + (y-k)^2 = r^2
-    %Equation 2 : y = ax + b, on néglige ici la composante Z puisque le cylindre est orienté vers le haut. 
-    %En d'autres mots, on applatit la situation dans la plan XY, pour avoir un cercle et une ligne
-    
-    intersectionExiste = true;
-    x = 0;
-    
-    penteXY = droite.PentePlanXY();
-    ordonneeAOrigine = droite.OrdonneeAOrigine();
-    a = 1 + power(penteXY, 2);
-    b = - (2 * cylindre.Centre.X) + 2 * penteXY * (ordonneeAOrigine - cylindre.Centre.Y);
-    c = power(cylindre.Centre.X, 2) + power(ordonneeAOrigine - cylindre.Centre.Y, 2) - power(cylindre.Rayon, 2);
-    
-    [x1, x2] = ResoudreEquationQuadratique(a, b, c);
-    
-    x1Valide = isreal(x1) && ((x1 > positionDepart.X && droite.Pente.X > 0) || (x1 < positionDepart.X && droite.Pente.X < 0));
-    x2Valide = isreal(x2) && ((x2 > positionDepart.X && droite.Pente.X > 0) || (x2 < positionDepart.X && droite.Pente.X < 0));
-    
-    if (x1Valide && x2Valide)
-        %Les 2 valeurs sont valides, on prend le x pour lequel on a un step positif
-        stepsX1 = (x1 - positionDepart.X) / droite.Pente.X;
-        stepsX2 = (x2 - positionDepart.X) / droite.Pente.X;
-
-        if (stepsX1 > 0 && stepsX1 < stepsX2)
-            x = x1;
-        else
-            x = x2;
-        end
-    elseif (x1Valide)
-        x = x1;
-    elseif (x2Valide)
-        x = x2;
-    else
-        intersectionExiste = false;
-    end
-    
-end
-
-
-function y = ComposanteYCollision(droite, x)
-    %y = ax+b
-    y = droite.PentePlanXY() * x + droite.OrdonneeAOrigine();
-end
-
-function z = ComposanteZCollision(droite, x)
-    %z = ax+b
-    z = droite.PentePlanXZ() * x + droite.ValeurZPourX0();
-end
-
-function  [x1, x2] = ResoudreEquationQuadratique(a, b, c)
-    %x = (-b +- (b^2 - 4ac)^(1/2)) / 2a
-    
-    x1 = (-b + sqrt(power(b, 2) - 4*a*c)) / (2*a);
-    x2 = (-b - sqrt(power(b, 2) - 4*a*c)) / (2*a);
 end
