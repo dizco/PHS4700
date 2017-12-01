1017db1e39dabd9ea6d7da487bffd988c2e9bbb2
dizco <gabriel.bourgault@kiosoft.ca>
2017-11-30 22:03:29 -0500

    Tests pour les collisions de bloc

---
+++ TP4/CollisionBlocSpec.m
@@ -0,0 +1,16 @@
+clc; 
+
+droite = Droite();
+droite.Point = Vecteur(2, 2, 10);
+droite.Pente = Vecteur(1.1, 1.1, 2);
+
+positionDepart = droite.Point;
+
+systeme = Donnees();
+
+[intersectionBlocExiste, positionIntersectionBloc, faceTouchee] = CollisionBloc(systeme, droite, positionDepart);
+
+fprintf('\n\n');
+fprintf('intersection : %d\n', intersectionBlocExiste);
+fprintf('position : [%d %d %d]\n', positionIntersectionBloc(1), positionIntersectionBloc(2), positionIntersectionBloc(3));
+fprintf('face : %d\n', faceTouchee);
